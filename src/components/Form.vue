d52d01d59ef114b5a49fa15f2adcd662113e8a26
Omar Gutiérrez <omargc.mus@gmail.com>
2018-01-12 00:15:21 -0400

    ejemplos con axios

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,21 +1,66 @@
 <template>
   <section>
-    <h2 class="title">Formulario</h2>
-    <form @submit.prevent="enviar()">
-      <input type="text"
-        class="form-control"
-        v-model="mensaje">
-        <button type="submit" class="btn">Agregar</button>
-    </form>
-    <h3>Listado</h3>
-    <ul v-if="lista.length > 0">
-      <li v-for="item in listaParesComputed">
-        {{ item }}
-      </li>
-    </ul>
-    <p v-else>
-      No tienes registros
-    </p>
+    <div class="grid cols-2">
+      <section>
+        <h2 class="title">Formulario</h2>
+        <form @submit.prevent="enviar()">
+          <div class="form-group">
+            <label>Mensaje</label>
+            <input type="text"
+              class="form-control"
+              v-model="mensaje">
+          </div>
+          <button type="submit" class="btn">Agregar</button>
+        </form>
+      </section>
+      <section>
+        <h2 class="title">Listado</h2>
+        <ul v-if="lista.length > 0">
+          <li v-for="item in listaParesComputed">
+            {{ item }}
+          </li>
+        </ul>
+        <p v-else>
+          No tienes registros
+        </p>
+      </section>
+    </div>
+
+    <h2 class="title">Post obtenido con axios</h2>
+    <div v-if="post">
+      <p><strong>Título: </strong>{{ post.title }}</p>
+      <p><strong>Contenido: </strong>{{ post.body }}</p>
+    </div>
+    <p v-else>Cargando post</p>
+
+    <h2 class="title">Lista de usuarios con axios</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nombre completo</th>
+          <th>Nombre de usuario</th>
+          <th>Correo electrónico</th>
+          <th>Dirección</th>
+          <th>Compañia</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in usuarios">
+          <td>{{ item.name }}</td>
+          <td>{{ item.username }}</td>
+          <td>{{ item.email }}</td>
+          <td>
+            {{ item.address.street }}
+            {{ item.address.suite }} -
+            {{ item.address.city }}
+          </td>
+          <td>
+            {{ item.company.name }} <br>
+            <small>{{ item.company.catchPhrase }}</small>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
@@ -27,16 +72,24 @@ export default {
     this.lista = [1, 2, 3, 4, 5, 6, 7, 8];
   },
   mounted () {
-    let root = 'https://jsonplaceholder.typicode.com';
-    axios.get(`${root}/posts/1`)
+    this.root = 'https://jsonplaceholder.typicode.com';
+
+    // Obteniendo post
+    axios.get(`${this.root}/posts/1`)
     .then(respuesta => {
-      console.log(respuesta.data);
+      console.log('Respuesta post:', respuesta.data);
+      this.post = respuesta.data;
     });
+
+    // Llamando a la lista de usuarios
+    this.listaUsuarios();
   },
   data () {
     return {
       mensaje: 'Hola mundo',
-      lista: []
+      lista: [],
+      post: null,
+      usuarios: null
     }
   },
   methods: {
@@ -46,6 +99,13 @@ export default {
     },
     listaPares () {
       return this.lista.filter(valor => valor % 2 == 0);
+    },
+    listaUsuarios () {
+      axios.get(`${this.root}/users`)
+      .then(respuesta => {
+        console.log('Respuesta usuarios:', respuesta.data);
+        this.usuarios = respuesta.data;
+      });
     }
   },
   computed: {
