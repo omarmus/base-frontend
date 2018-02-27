39fae22b338672ab934c59e86d07601381ef3cc0
mcallisaya <mcallisaya@agetic.gob.bo>
2018-02-27 19:02:27 -0400

    crud

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,43 @@
 <template>
   <section>
-    Taller de frontend
+    <h2 class="title">Formulario</h2>
+    <h3>Número: {{ numero }}</h3>
+    Ingrese número: <input type="text" v-model="numero">
+    <button type="button" @click="agregar()" class="btn btn-primary">Agregar</button>
+    <ul v-if="lista.length > 0">
+      <li v-for="item in lista">{{ item }}</li>
+    </ul>
+    <div v-else>
+      La lista no tiene elementos
+    </div>
+    <button class="btn btn-primary" @click="sumar">Sumar números</button>
+    <h3>Suma total: {{ suma }}</h3>
   </section>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      mensaje: 'Hola mundo',
+      numero: 0,
+      lista: [],
+      suma: 0
+    }
+  },
+  methods: {
+    agregar () {
+      let numero = this.numero;
+      this.lista.push(numero)
+    },
+    sumar () {
+      let suma = 0;
+      for (let i in this.lista) {
+        suma += parseInt(this.lista[i]);
+      }
+      this.suma = suma;
+    }
+  }
+};
+</script>
---
+++ src/components/Lista.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <h3>Empresas</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Dirección</th>
+            </tr>
+            <tr v-for="item in empresas">
+                <td>{{ item.nombre }}</td>
+                <td>{{ item.direccion }}</td>
+            </tr>
+        </table>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created () {
+        this.url = 'http://localhost:4000/api-rest/';
+        axios.get(`${this.url}empresas`)
+        .then((respuesta) => {
+            console.log(respuesta.data);
+            this.empresas = respuesta.data;
+        })
+    },
+    mounted () {
+
+    },
+    data () {
+        return {
+            empresas: []
+        }
+    }
+}
+</script>
